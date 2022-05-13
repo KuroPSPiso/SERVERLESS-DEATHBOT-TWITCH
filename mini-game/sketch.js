447f01df95b27028a8ba14731877819bebfee4b8
Ryan van den Bogaard <bogaardryan@hotmail.com>
2022-05-14 00:33:11 +0200

    Basics ready

--- mini-game/sketch.js
+++ mini-game/sketch.js
@@ -1,3 +1,9 @@
+let theme;
+var deathLaugh;
+var deathSound;
+var missSound;
+var critSound;
+var atkSound;
 let gameOverIcon = {
   "frameIndex": -1,
   "frames" : []
@@ -117,29 +123,43 @@ function preload() {
   gameOverIcon.frames.push(img4);
   gameOverIcon.frames.push(img3);
   if(gameOverIcon.frames.length > 0) gameOverIcon.frameIndex = 0;
+  
+  soundFormats('ogg', 'mp3');
+  theme = loadSound('https://bogaardryan.com/client-side-bs-app/assets/pakimon2.mp3');
+  deathLaugh = loadSound('https://bogaardryan.com/client-side-bs-app/assets/death-laugh.mp3');
+  deathSound = loadSound('https://bogaardryan.com/client-side-bs-app/assets/death.mp3');
+  missSound = loadSound('https://bogaardryan.com/client-side-bs-app/assets/miss.mp3');
+  atkSound = loadSound('https://bogaardryan.com/client-side-bs-app/assets/atk.mp3');
+  critSound = loadSound('https://bogaardryan.com/client-side-bs-app/assets/crit.mp3');
 }
 
 function setup() {
+  font = loadFont('https://bogaardryan.com/client-side-bs-app/assets/Regular.otf');
   createCanvas(400, 200);
   player1.img = loadImage('https://bogaardryan.com/client-side-bs-app/assets/ratgeRearx2.png');
   player2.img = loadImage('https://bogaardryan.com/client-side-bs-app/assets/ratgeFrontx2.png');
+
+  start("Debug_1", "kur0")
 }
 
-function shakePlayer(player, playerShake)
+function shakePlayer(player, playerShake, player2 = false)
 {
   if(playerShake.shake > 0 && playerShake.sine <= 0)
   {
     playerShake.sine = 1;
     playerShake.shake--;
   }
-  
-  player.offset.x = -0 + Math.sin((playerShake.sine/0.5 >= 1? 1-playerShake.sine : playerShake.sine) * 2) * 20;
+  if(player2 === true)
+  {
+    player.offset.x = 0 - Math.sin((playerShake.sine/0.5 >= 1? 1-playerShake.sine : playerShake.sine) * 2) * 20;
+  }  
+  else player.offset.x = -0 + Math.sin((playerShake.sine/0.5 >= 1? 1-playerShake.sine : playerShake.sine) * 2) * 20;
   playerShake.sine -= deltaTime * playerShake.strength;
   if(playerShake.sine < 0) playerShake.sine = 0;
   if(playerShake.sine == 0) player.offset.x = 0
+
 }
 
-let flash = 1;
 function finishFlash() {
   if(flash <= 0) return true;
   let color = Math.sin(flash*16);
@@ -170,53 +190,94 @@ function scrollInPlayers(){
 }
 
 function canPlay(){
-  
   if(finishFlash() === true &&
      scrollInPlayers() === true
     ) return true;
   return false;
 }
 
-let playerTurn = Math.random() < 0.5;
-const turnTime = 2;
-let turnTimer = 0;
+var attack = {
+  type: "",
+  strength: 0.1
+}
 
 function turn(){
   turnTimer -= deltaTime * 0.001;
+
+  var attackResponseText;
+  switch(attack.type)
+  {
+    case "miss": 
+      attackResponseText = "'s attack missed!";
+      break;
+    case "atk": 
+      attackResponseText = " wrote Ratge";
+      break;
+    case "crit": 
+      attackResponseText = " wrote RatJAMMM, A critical hit!";
+      break;
+  }
+
+  attackText = `${playerTurn === true? player1.name : player2.name }${attackResponseText}`;
+
   if(turnTimer <= 0){
+    
+    var attackPct = Math.random() * 100;
+    if(attackPct < 20)
+    {
+      attack.type = "miss"
+      missSound.play();
+    }
+    else if(attackPct < 90)
+    {
+      attack.type = "atk"
+      atkSound.play();
+      attack.strength = 0.05 + Math.random() * 0.05;
+    }
+    else
+    {
+      attack.type = "crit"
+      critSound.play();
+      attack.strength = 0.01 + Math.random() * 0.1;
+    }
+
     turnTimer = turnTime;
     playerTurn = !playerTurn;
-    if(playerTurn === true){
-      player1Shake.shake = 1
-    } else {
-      player2Shake.shake = 1
+    
+    if(attack.type != "miss")
+    {
+      if(playerTurn === true){
+        player1Shake.shake = 1
+      } else {
+        player2Shake.shake = 1
+      }
     }
   }
   
-  if(turnTimer <= 1.5 && turnTimer >= 1.4){
-    if(playerTurn === true){
-      player2Shake.shake = 2
-    } else {
-      player1Shake.shake = 2
+  if(attack.type != "miss")
+  {
+    if(turnTimer <= 1.5 && turnTimer >= 1.4){
+      if(playerTurn === true){
+        player2Shake.shake = 2
+      } else {
+        player1Shake.shake = 2
+      }
     }
   }
   
-  textSize(12);
-  textAlign(LEFT);
-  text(`${playerTurn === true? player1.name : player2.name } attacks with`, 10, 178);
+  var additionalStrenght = 1;
+  if(attack.type === "crit") additionalStrenght = 2;
+
   if(playerTurn === true){
     //player 1
-    player1Shake.strength = shakeStrenghtAttack
-    player2Shake.strength = shakeStrenghtDamaged
+    player1Shake.strength = shakeStrenghtAttack * additionalStrenght
+    player2Shake.strength = shakeStrenghtDamaged * additionalStrenght
   } else {
     //player 2
-    player1Shake.strength = shakeStrenghtDamaged
-    player2Shake.strength = shakeStrenghtAttack
+    player1Shake.strength = shakeStrenghtDamaged * additionalStrenght
+    player2Shake.strength = shakeStrenghtAttack * additionalStrenght
   }
 }
-
-var gameOverTime = 5;
-var gameOverTimer = gameOverTime;
 
 function drawGameOver()
 {
@@ -224,51 +285,143 @@ function drawGameOver()
       image(gameOverIcon.frames[gameOverIcon.frameIndex], 100, 100);
     }
   
-    gameOverIcon.frameIndex = Math.ceil( Math.abs(gameOverTimer) * 20 % gameOverIcon.frames.length - 1); //10fps
+    gameOverIcon.frameIndex = Math.ceil( Math.abs(gameOverDrawTimer) * 20 % gameOverIcon.frames.length - 1); //10fps
   
+    noStroke()
+    textFont(font);
     textAlign(CENTER);
-    textSize(20);
-    text(`${player1.hp <= 0? player1.name : player2.name } fainted`, 200, 100)
+    textSize(14);
+    text(`"${player1.hp <= 0? player1.name : player2.name }" \n\nwill receive a ban`.toUpperCase(), 200, 60)
+    gameOverDrawTimer -= deltaTime * 0.001;
+
+}
+
+function gameOver(player)
+{
+  theme.stop();
+
+  if(gameOverTime === gameOverTimer)
+  { 
+    deathSound.play();
+    attackText = `${player.name} fainted!`
+  }
+
+  if(gameOverTimer > 0)
+  {
     gameOverTimer -= deltaTime * 0.001;
+    player.offset.x = 0;
+    player.offset.y += deltaTime * 0.3;
+    return false;
+  } 
+  if(gameOverDrawTime === gameOverDrawTimer) deathLaugh.play();
+  drawGameOver();
+  gameOverAlertScreenTimer -= deltaTime * 0.001;
+  if(gameOverAlertScreenTimer < 0) reset();
+  return true;
 }
 
 function draw() {
+  background(0, 255, 0);
+
+  if(ready === false) return;
   background(255, 255, 255);
-  
+  stroke(1)
+
   if(player1.hp > 0 && player2.hp > 0){
-    fill(color(0,0,0));
-    rect(0, 0, 2, 200);
-    rect(398, 0, 2, 200);
-    rect(0, 163, 400, 1);
     if(canPlay() === false) return;
     turn();
   } else {
-    drawGameOver();
-    return;
+    if(gameOver(player1.hp > player2.hp? player2 : player1) === true) return;
   }
   
   shakePlayer(player1, player1Shake);
-  shakePlayer(player2, player2Shake);
+  shakePlayer(player2, player2Shake, true);
   
   image(player1.img, player1.square.x + player1.offset.x, player1.square.y + player1.offset.y, player1.square.size, player1.square.size);
   image(player2.img, player2.square.x + player2.offset.x, player2.square.y + player2.offset.y, player2.square.size, player2.square.size);
   
+  noStroke()
+  fill(color(255,255,255));
+  rect(player1.square.x, player1.square.y + player1.square.size, 200, 200)
+  rect(player2.square.x, player2.square.y + player2.square.size, 200, 200)
+
+  stroke(1)
+  fill(color(0,0,0));
+  rect(0, 0, 2, 200);
+  rect(398, 0, 2, 200);
+  rect(0, 163, 400, 1);
   
+  fill(color(0,0,0));
   //health bars
   //p2
+  stroke(1)
   rect(30, 65, 150, 1);
   rect(29, 35, 1, 30);
-  textSize(16);
+  noStroke()
+  textFont(font);
+  textSize(11);
   textAlign(LEFT);
-  text(player2.name, 20, 15);
+  text(player2.name.toUpperCase(), 20, 15);
   //p1
+  stroke(1)
   rect(200, 150, 150, 1);
   rect(350, 120, 1, 30);
+  noStroke()
+  textFont(font);
   textAlign(RIGHT);
-  textSize(16);
-  text(player1.name, 360, 105);
+  textSize(11);
+  text(player1.name.toUpperCase(), 360, 105);
+  
+  //attack info
+  noStroke()
+  textFont(font);
+  textSize(10);
+  textAlign(LEFT);
+  text(attackText.toUpperCase(), 10, 188);
   
   if(mouseIsPressed){
-    player1.hp = 0
+    //player1.hp = 0
+    player2.hp = 0
   }
+}
+
+
+let playerTurn = Math.random() < 0.5;
+const turnTime = 2;
+let turnTimer = 0;
+let attackText = "";
+
+let flashTime = 2;
+let flash = flashTime;
+
+var gameOverDrawTime = 15;
+var gameOverDrawTimer = gameOverDrawTime;
+
+var gameOverTime = 2;
+var gameOverTimer = gameOverTime;
+var gameOverAlertScreenTime = 15;
+var gameOverAlertScreenTimer = gameOverAlertScreenTime;
+
+let ready = false;
+let font;
+
+function reset()
+{
+  flash = flashTime;
+  gameOverDrawTimer = gameOverDrawTime;
+  gameOverTimer = gameOverTime;
+  gameOverAlertScreenTimer = gameOverAlertScreenTime;
+  turnTimer = 0
+  playerTurn = Math.random() < 0.5;
+  attackText = "";
+  ready = false;
+}
+
+function start(name1, name2)
+{
+  reset();
+  ready = true;
+  theme.loop()
+  player1.name = name1
+  player2.name = name2
 }
